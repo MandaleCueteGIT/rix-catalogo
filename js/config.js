055aad1ce970a8d86c88c638739b1a34606618e6
MandaleCueteGIT <lautaro_alto@hotmail.com>
2026-09-09 16:08:26 -0300

    feat: conectar catálogo con RIX API

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window.RIX_CONFIG = {
-  VERSION: '0.1.0',
-  API_URL: '',
-  DEMO_MODE: true,
+  VERSION: '0.2.0',
+  API_URL: 'https://script.google.com/macros/s/AKfycbzYBulNNcjFUhITWSglaXfJXXLXagLjLqbLo55xFXmAj-qjewqqORSUIyEptCCMjYMG3Q/exec',
+  DEMO_MODE: false,
   CURRENCY: 'ARS',
   STORAGE_KEY: 'rix_catalogo_cart_v1'
 };
